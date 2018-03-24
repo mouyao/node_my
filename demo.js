3e709ea421ff404b584f9461f34966df9601e6ee
mouyao <729329769@qq.com>
2018-03-24 17:20:23 +0800

    第一次将自己写的nodejs开发后台的代码上传

---
+++ demo.js
@@ -0,0 +1,24 @@
+var mysql  = require('mysql');  
+ 
+var connection = mysql.createConnection({     
+      host     :"localhost",
+     user     : 'root',
+     password : '20180324',
+     database: "mysql"
+}); 
+ 
+connection.connect();
+ 
+var  sql = 'SELECT * FROM `mysql` .mouyao_test order by id desc';
+//查
+connection.query(sql,function (err, result) {
+        if(err){
+          console.log('[SELECT ERROR] - ',err.message);
+          return;
+        }
+       console.log('--------------------------SELECT----------------------------');
+       console.log(result);
+       console.log('------------------------------------------------------------\n\n');  
+});
+ 
+connection.end();
